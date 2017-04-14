a950b95012c55d69309a88f13e4dae4960bee365
Vasisht Duddu <vduddu@protonmail.com>
2017-04-14 19:12:36 +0530

    Add files via upload

---
+++ Misc/hacked_sites.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+sudo pip install shodan
+echo "Enter you API Key"
+read api
+shodan init $api
+echo "Enter the total results you want to get:"
+read limitvar
+echo $limitvar
+#shodan download --limit $limitvar hacked 'title:"hacked by"'
+shodan parse --fields ip_str,port,org,hostnames hacked.json.gz
+echo "#########################################################"
+echo "#################Names of Hackers########################"
+echo "#########################################################"
+shodan parse --fields title hacked.json.gz | grep -i "hacked by" | sed -e 's/.*hacked by//' 
